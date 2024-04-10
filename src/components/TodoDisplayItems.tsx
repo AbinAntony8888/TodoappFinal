7f166110a7fe29673e45abb5acfad3cab81e1649
AbinAntony8888 <119113517+AbinAntony8888@users.noreply.github.com>
2024-04-10 19:28:36 +0530

    first commit

--- src/components/TodoDisplayItems.tsx
+++ src/components/TodoDisplayItems.tsx
@@ -14,16 +14,9 @@ export default function TodoDisplayItems({ todoList, setTodoList }: todoType) {
   const clearAll = () => {
     setTodoList([]);
   };
-  // const editItem=(event:number)=>{
-  //   const editTodoArray = [...todoList]; 
-  // }
-
-  const editItem = (index: number) => {
-    // Enable editing for the item at the given index
-    const newTodoList = [...todoList];
-    newTodoList[index] = prompt('Enter the new task:', todoList[index]) || todoList[index];
-    setTodoList(newTodoList);
-  };
+  const editItem=(event:number)=>{
+    const newTodo = [...todoList]; 
+  }
 
   return (
     <div>
@@ -35,16 +28,9 @@ export default function TodoDisplayItems({ todoList, setTodoList }: todoType) {
               type="text"
               value={item}
               id="inputdisplay"
-              // onChange={(e) => editItem(index, e.target.value)}
               className="bg-teal-100  border-2 border-black p-1 text-amber-950"
             /> 
-            {/* <button
-              onClick={() => deleteItem(index)}
-              className="bg-[red] p-1 m-1"
-            >
-              Delete
-            </button> */}
-            {/* <button className="bg-[#38f351] p-1 m-1 px-4">Edit</button> */}
+          
             <TodoButton
               value={"Delete"}
               colour={"bg-[red]"}
@@ -53,12 +39,6 @@ export default function TodoDisplayItems({ todoList, setTodoList }: todoType) {
             <TodoButton value={"Edit"} colour={"bg-[#6AB20D]" } buttonClick={()=>editItem(index)} />
           </li>
         ))}
-        {/* <button
-          onClick={clearAll}
-          className="bg-[#864747] p-1 m-1 px-4 text-white"
-        >
-          Clear All
-        </button> */}
         <TodoButton 
           value={"clear all"}
           colour={"bg-[#cb3333]"}
